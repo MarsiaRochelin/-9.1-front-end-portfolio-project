fca858158e51e0c5f3bc3dbd4fdd7b365815a743
MarsiaRochelin <marsrochelin@pursuit.org>
2022-09-09 10:05:53 -0400

    proposal

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-const URL = ""
+const URL = "https://pursuit-9-1-full-stack-project.herokuapp.com/api/colors"
 
-fetch("")
-.then(res => res.json) 
+fetch(URL)
+.then(res => res.json()) 
 .then(data => console.log(data))
-.catch(err => console.log(err)
+.catch(err => console.log(err))
+
+//colors
+// const black = data[0]['color']
+// const white = data[1]['color']
+// const red = data[2]['color']
+// const blue = data[3]['color']
+// const yellow = data[4]['color']
+// const green = data[5]['color']
+
