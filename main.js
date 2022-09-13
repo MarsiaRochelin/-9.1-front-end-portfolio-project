3beedd30f4bfe0ce6333ec1f91d3bfd47b8a9627
MarsiaRochelin <marsrochelin@pursuit.org>
2022-09-12 22:26:21 -0400

    updates

--- main.js
+++ main.js
@@ -26,17 +26,11 @@ function colorInfo(userInput){
     const blue = data[3]['color']
     const yellow = data[4]['color']
     const green = data[5]['color']
-
-    //create an if/else if statements for each color in the selcetion to account for redirecting user to different pages based on their selection
-        //each page should have a paragraph describing the psychology of it
-        //each page should have a media query that changes the font-family, text color, and/or text size change depending on my screen size. 
+        
     //else statement can account for error message handling for color selections not avaialable
-
-    // const newPages = document.querySelector('.selection')
     
     if(userInput === black){
         const blackPsychologhy = document.querySelector('#black')
-        //add a hover to the text 
         blackPsychologhy.textContent = 'BLACK'
     } else if(userInput === white){
         const whitePsychologhy = document.querySelector('#white')
